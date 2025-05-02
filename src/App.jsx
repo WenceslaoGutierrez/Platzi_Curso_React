dee6deaa74b39b2de6888fafc21b823300591948
Wenceslao <wences_gv@hotmail.com>
2025-05-01 18:49:54 -0600

    Creacion de contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Card from './components/Card'
+import Counter from './components/Counter'
 import './App.css'
 
 function App() {
-  const items = ["React","Django","Java","JavaScript","Vite"];
 
   return (
-    <section>
-      <h1>¡Hola mundo!</h1>
-      <Card title="Card 1" description="Card description"></Card>
-      <Card title="Card 2" description="Card description"></Card>
-      <ul>
-        {
-          items.map((item, index)=>(
-            <li key={index}>{item}</li>
-          ))
-        }
-      </ul>
-    </section>
+    <>
+      <Counter />
+    </>
   );
 }
 
