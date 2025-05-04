13e58a29a912e83de1ee2f77a289e79ae2d4eccb
Wenceslao <wences_gv@hotmail.com>
2025-05-04 14:18:24 -0600

    Creación de TailwindText usando Tailwind para estilos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Card from './components/Card/Card'
 import './App.css'
+import TailwindText from './components/Card/Text/TailwindText'
+
 
 function App() {
 
   return (
     <>
-      <Card title="Card" imgUrl="https://i.redd.it/fq1cdp5dmim71.jpg" 
-      description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."/>
+      <TailwindText></TailwindText>
     </>
   );
 }
