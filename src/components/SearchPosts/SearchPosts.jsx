53b736d7b66fc5e2270f941ee802192fbca3508b
Wenceslao <wences_gv@hotmail.com>
2025-05-04 21:44:44 -0600

    Creación de SearchPosts para hacer busquedas usando query y fetch.

--- src/components/SearchPosts/SearchPosts.jsx
+++ src/components/SearchPosts/SearchPosts.jsx
@@ -1,5 +1,36 @@
-const SearchPosts = () => {
+import { useState, useEffect } from "react";
 
+const SearchPosts = () => {
+    const [query,setQuery] = useState("");
+    const [posts,setPosts] = useState([]);
+
+    useEffect(() => {
+            fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${query}`)
+                .then((response) => response.json())
+                .then((data) => setPosts(data))
+                .catch((error) => console.error("Error fetching data: ", error))
+    },[query])
+
+    return(
+        <>
+            <div className="p-6 max-w-4xl mx-auto">
+                <h1 className="text-2xl font-medium mb-4 text-center">Dynamic query</h1>
+                <h2 className="text-2xl font-bold mb-4 text-center">Post List</h2>
+                <input type="text" 
+                    placeholder="Search by title" value={query} 
+                    className="mb-6 w-full border rounded px-4 py-2"
+                    onChange={(event) => setQuery(event.target.value)}/>
+                <ul className="space-y-4">
+                    {posts.map((post) => (
+                    <li key={post.id} className="bg-white shadow-md rounded-xl p-4 border hover:shadow-lg transition">
+                        <h2 className="text-xl font-semibold text-gray-800">{post.title}</h2>
+                        <p className="text-sm text-gray-600">Content: {post.body}</p>
+                    </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default SearchPosts;
---
+++ src/components/Text/TailwindText.jsx
@@ -0,0 +1,7 @@
+const TailwindText = () =>{
+    return(
+        <h1 className="text-3xl font-bold underline text-violet-950"> Hello Tailwind!</h1>
+    )
+}
+
+export default TailwindText;
